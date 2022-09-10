6a08327deec7f4abcf7f9f9b1ae7baad23da80ab
xiaozhou1000 <108339718+xiaozhou1000@users.noreply.github.com>
2022-09-10 13:52:33 +0800

    Update lovetime.js

--- js/lovetime.js
+++ js/lovetime.js
@@ -12,7 +12,7 @@ function lovetime() {
     var todayHour = today.getHours()
     var todayMinute = today.getMinutes()
     var todaySecond = today.getSeconds()
-    var t1 = Date.UTC(2022, 08, 11, 02, 02, 33)
+    var t1 = Date.UTC(2022, 08, 12, 02, 02, 33)
     var t2 = Date.UTC(todayYear, todayMonth, todayDate, todayHour, todayMinute, todaySecond)
     var diff = t2 - t1
     var diffYears = Math.floor(diff / years)
